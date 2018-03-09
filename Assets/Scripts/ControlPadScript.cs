7fc9d75dade22bd3624d8746daf4ab5471ec5dbf
Nigel <28766663+nigel5@users.noreply.github.com>
2018-03-09 18:43:07 -0500

    add scripts

---
+++ Assets/Scripts/ControlPadScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPadScript : MonoBehaviour {
+
+    Camera mCamera;
+    int iColour = 0;
+    Image image;
+
+    Color[] colours = new Color[4] { Color.white, Color.red, Color.green, Color.blue };
+
+	// Use this for initialization
+	void Start () {
+        mCamera = Camera.main;
+        image = GetComponent<Image>();
+        // Set scaling so that the control pad spans the width of the screen and correct height
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.OnTap();
+        }
+	}
+
+    void OnTap()
+    {
+        iColour += 1;
+        image.color = colours[iColour];
+        if (iColour >= colours.Length - 1)
+        {
+            iColour = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpriteScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpriteScript : MonoBehaviour {
+
+	public int health;
+	public int shield;
+	public int speed;
+
+	// 1: Straight down
+	// 2: Diagonal
+	// 3: Crazy
+	public int movementType;
+	Vector2 movementDir;
+
+	public bool canShoot;
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+		if (movementType == 1) {
+			movementDir = Vector2.down;
+		}
+		else
+        {
+            // TODO implement different movement types
+            movementDir = Vector2.down;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+        {
+            Destroy(this);
+        }
+
+        gameObject.transform.Translate(movementDir * Time.deltaTime);
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+    }
+
+}
